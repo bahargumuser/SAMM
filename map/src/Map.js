74dac6282d914973590675f7485a56bb2357bf1f
Bahar Gümüşer <121288876+bahargumuser@users.noreply.github.com>
2023-09-27 00:27:36 +0300

    add external state, buttons

--- map/src/Map.js
+++ map/src/Map.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState, useCallback, useEffect, useMemo } from 'react';
 import { MapContainer, Marker, TileLayer, Popup } from 'react-leaflet';
 import "leaflet/dist/leaflet.css";
 import { Icon } from 'leaflet';
 
 
   function Map() {
+
+    const center = [48.5866, 2.3522];
+    const zoom = 13;
 
 
     const markers = [
@@ -25,26 +28,40 @@ import { Icon } from 'leaflet';
 const customIcon = new Icon({
     iconUrl : "https://cdn-icons-png.flaticon.com/512/5591/5591266.png",
     iconSize : [38, 38]
-})
+});
 
+const [position, setPosition] = useState(center);
+
+const onClick = useCallback(() => {
+  setPosition(center);
+}, [center]);
+
+const map = useMemo(() => (
+  [center, zoom]));
 
     return (
-        <MapContainer center={[48.5866, 2.3522]} zoom={13}>
-            <TileLayer
-            attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-                {
-                    markers.map(marker => (
-                            <Marker position={marker.geocode} icon={customIcon}>
-                                <Popup>
-                                    <h2>You are here now</h2>
-                                </Popup>
-                            </Marker>
-                    ))
-                }
+     <div>
+        
+      <MapContainer center={center} zoom={zoom} >
+    <TileLayer
+          attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+              />
+                { markers.map(marker => (
+                          <Marker position={marker.geocode} icon={customIcon} key={marker.geocode.join(',')}>
+                              <Popup>
+                                  <h2>{marker.popUp}</h2>
+                              </Popup>
+                          </Marker>
+                  ))}
       </MapContainer>
-    );
+      <p>
+        latitude: {position[0].toFixed(4)}, longitude: {position[1].toFixed(4)}{' '}
+        <button onClick={onClick}>reset</button>
+        <button onClick={onClick}>noktayı kaydet</button>
+      </p>
+     </div>
+    )
   }
 
 export default Map;
