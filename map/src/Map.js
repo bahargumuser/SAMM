6b3728e6b242b3d0721ffa2f6c8696379959cbf6
Bahar Gümüşer <121288876+bahargumuser@users.noreply.github.com>
2023-09-27 18:44:11 +0300

    completed external state

--- map/src/Map.js
+++ map/src/Map.js
@@ -1,67 +1,64 @@
 import React, { useState, useCallback, useEffect, useMemo } from 'react';
 import { MapContainer, Marker, TileLayer, Popup } from 'react-leaflet';
 import "leaflet/dist/leaflet.css";
-import { Icon } from 'leaflet';
 
+function Map() {
+  const [map, setMap] = useState(null);
+  const [position, setPosition] = useState([0, 0]);
 
-  function Map() {
+  const center = [48.5866, 2.3522];
+  const zoom = 13;
 
-    const center = [48.5866, 2.3522];
-    const zoom = 13;
+  const onClick = useCallback(() => {
+    if (map) {
+      map.setView(center, zoom);
+    }
+  }, [map, center, zoom]);
 
-
-    const markers = [
-        {
-          geocode: [48.86, 2.3522],
-          popUp: "Hello, I am pop up 1"
-        },
-        {
-          geocode: [48.85, 2.3522],
-          popUp: "Hello, I am pop up 2"
-        },
-        {
-          geocode: [48.855, 2.34],
-          popUp: "Hello, I am pop up 3"
-        }
-      ];
-
-const customIcon = new Icon({
-    iconUrl : "https://cdn-icons-png.flaticon.com/512/5591/5591266.png",
-    iconSize : [38, 38]
-});
-
-const [position, setPosition] = useState(center);
-
-const onClick = useCallback(() => {
-  setPosition(center);
-}, [center]);
-
-const map = useMemo(() => (
-  [center, zoom]));
-
-    return (
-     <div>
-        
-      <MapContainer center={center} zoom={zoom} >
-    <TileLayer
-          attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
-              />
-                { markers.map(marker => (
-                          <Marker position={marker.geocode} icon={customIcon} key={marker.geocode.join(',')}>
-                              <Popup>
-                                  <h2>{marker.popUp}</h2>
-                              </Popup>
-                          </Marker>
-                  ))}
-      </MapContainer>
-      <p>
-        latitude: {position[0].toFixed(4)}, longitude: {position[1].toFixed(4)}{' '}
-        <button onClick={onClick}>reset</button>
-        <button onClick={onClick}>noktayı kaydet</button>
-      </p>
-     </div>
-    )
+  const onMove = useCallback(() => {
+    if (map) {
+      const center = map.getCenter();
+    if (center) {
+      setPosition([center.lat, center.lng]);
+    }
   }
+  }, [map]);
+  
+  useEffect(() => {
+    console.log(map);
+    if (map) {
+       
+      map.on('move', onMove);
+   
+      return () => {
+        map.off('move', onMove);
+
+      };
+    }
+  }, [map, onMove]);
+
+  const displayMap = useMemo(() => (
+    <MapContainer center={center} zoom={zoom} ref={setMap}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+   
+    </MapContainer>
+  ), [center, zoom]);
+
+  return (
+    <div>
+      {map && position ? (
+        <p>
+          latitude: {position[0].toFixed(4)}, longitude: {position[1].toFixed(4)}{' '}
+          <button onClick={onClick}>reset</button>
+          <button onClick={onClick}>noktayı kaydet</button>
+        </p>
+      ) : null}
+      {displayMap}
+    </div>
+  );
+}
 
 export default Map;
