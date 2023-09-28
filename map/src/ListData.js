3778da1e82468f9ee41d700ae8236e3a7b818025
Bahar Gümüşer <121288876+bahargumuser@users.noreply.github.com>
2023-09-29 01:56:58 +0300

    add delete, add css

--- map/src/ListData.js
+++ map/src/ListData.js
@@ -1,18 +1,44 @@
 import React from 'react';
+import axios from 'axios';
 
-function ListData({ data }) {
+function ListData({ data, setClickPoint }) {
+  const handleDelete = async (itemId) => {
+    try {
+      const response = await axios.post("http://localhost:3001/sil", { id: itemId });
+      console.log("Silindi", response.data);
+    } catch (error) {
+      console.error('Silme hatası:', error);
+    }
+  };
+
   return (
     <div>
-      <h2>Saved Points</h2>
       <ul>
         {data.map((item, index) => (
           <li key={index}>
-            Latitude: {item.latitude}, Longitude: {item.longitude}, Date: {item.date}
+            Latitude: {item.lat.toFixed(2)}, Longitude: {item.lng.toFixed(2)}, Date: {item.datetime}
+            <button onClick={() => handleDelete(item.id) } style={{
+                backgroundColor: "#f5f5f5", 
+                padding: "5px 10px", 
+                borderRadius: "10px", 
+                marginRight: "10px", 
+                marginBottom: "10px", 
+                cursor: "pointer",
+              }}>Sil</button>
+              
+            <button onClick={() => setClickPoint([item.lat, item.lng])} style={{
+                backgroundColor: "#f5f5f5", 
+                padding: "5px 10px", 
+                borderRadius: "10px", 
+                marginRight: "10px", 
+                marginBottom: "10px", 
+                cursor: "pointer",
+              }}> Noktaya Git</button>
+
           </li>
         ))}
       </ul>
     </div>
   );
 }
-
 export default ListData;
