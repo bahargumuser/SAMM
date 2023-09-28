204665e4baebfafbedd943958a64be89c9df78e7
Bahar Gümüşer <121288876+bahargumuser@users.noreply.github.com>
2023-09-29 01:56:24 +0300

    contact be, add point, css

--- map/src/Map.js
+++ map/src/Map.js
@@ -1,14 +1,22 @@
-import React, { useState, useCallback, useEffect, useMemo } from 'react';
-import { MapContainer, TileLayer } from 'react-leaflet';
+import React, { useState, useCallback, useEffect, useMemo, useRef } from "react";
+import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import L from 'leaflet';
 import "leaflet/dist/leaflet.css";
-import axios from 'axios';
-import ListData from './ListData'; 
+import axios from "axios";
+import ListData from "./ListData";
 
 function Map() {
-  
   const [map, setMap] = useState(null);
+  const mapRef = useRef(null);
   const [position, setPosition] = useState([0, 0]);
   const [savedPoints, setSavedPoints] = useState([]);
+    const [clickPoint, setClickPoint] = useState([48.5866, 2.3522]);
+
+  const crosshairIcon = new L.Icon({
+    iconUrl: './maps-and-flags.png',
+    iconSize: [20, 20],
+    iconAnchor: [10, 10],
+  });
 
   const center = [48.5866, 2.3522];
   const zoom = 13;
@@ -22,27 +30,11 @@ function Map() {
   const onMove = useCallback(() => {
     if (map) {
       const center = map.getCenter();
-    if (center) {
-      setPosition([center.lat, center.lng]);
+      if (center) {
+        setPosition([center.lat, center.lng]);
+      }
     }
-  }
   }, [map]);
-
-  const saveUrl = 'http://localhost:3001/kaydet';
-  const downloadUrl = 'http://localhost:3001/indir';
-  
-  useEffect(() => {
-    console.log(map);
-    if (map) {
-       
-      map.on('move', onMove);
-   
-      return () => {
-        map.off('move', onMove);
-
-      };
-    }
-  }, [map, onMove]);
 
   const handleSaveClick = async () => {
     if (position && position.length === 2) {
@@ -50,75 +42,121 @@ function Map() {
       const datetime = new Date().toISOString();
       try {
         const response = await axios.post(saveUrl, { lat, lng, datetime });
-        console.log('Nokta kaydedildi:', response.data);
+        console.log("Nokta kaydedildi:", response.data);
         setSavedPoints([...savedPoints, response.data]);
+        console.log(savedPoints);
       } catch (error) {
-        console.error('Kaydetme hatası:', error);
+        console.error("Kaydetme hatası:", error);
       }
     }
   };
 
+  const saveUrl = "http://localhost:3001/kaydet";
+  const downloadUrl = "http://localhost:3001/indir";
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("http://localhost:3001/veriler");
+        if (response.data) {
+          setSavedPoints(response.data);
+        } else {
+          console.error("API yanıtı boş veya beklenen veri yapıda değil.");
+        }
+      } catch (error) {
+        console.error("Veri alınırken bir hata oluştu:", error);
+      }
+    };
+    fetchData();
+  }, [savedPoints]);
+  useEffect(() => {
+    console.log(map);
+    if (map) {
+      map.on("move", onMove);
+      return () => {
+        map.off("move", onMove);
+      };
+    }
+  }, [map, onMove]);
   const handleDownloadClick = () => {
-    // JSON dosyasını indirme
     window.location.href = downloadUrl;
   };
 
-  const displayMap = useMemo(() => (
-    <MapContainer center={center} zoom={zoom} ref={setMap}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-   
-    </MapContainer>
-  ), [center, zoom]);
+
+  const displayMap = useMemo(
+    () => (
+      <MapContainer center={clickPoint} zoom={zoom} ref={setMap}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {map && <Marker position={clickPoint} icon={crosshairIcon} clickable={false} />}
+      </MapContainer>
+    ),
+    [center, zoom, clickPoint]
+  );
+
+  useEffect(() => {
+    if (mapRef.current) {
+      setMap(mapRef.current.leafletElement);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (map) {
+      map.setView(clickPoint, zoom);
+    }
+  }, [map, clickPoint, zoom]);
+
 
   return (
     <div>
-  
       <div className="sidebar">
-     
         <div
           style={{
-            position: 'fixed',
-            top: '10px',
-            right: '10px',
-            zIndex: '1000',
-            border: '0.5vh solid #000',
-            padding: '2.8vh',
-            width: '50vh',
-            height: '200px',
-            backgroundColor: '#fff',
+            position: "fixed",
+            top: "10px",
+            right: "10px",
+            zIndex: "1000",
+            border: "0.5vh solid #000",
+            padding: "2.8vh",
+            width: "50vh",
+            backgroundColor: "#fff",
           }}
         >
            {map && position ? (
-        <p>
-          latitude: {position[0].toFixed(4)}, longitude: {position[1].toFixed(4)}{' '}
-          <button onClick={onClick}>reset</button>
-        </p>
-      ) : null}
-          <ListData data={savedPoints} />
+            <p>
+              latitude: {position[0].toFixed(4)}, longitude:{" "}
+              {position[1].toFixed(4)} <button onClick={onClick} >reset</button>
+            </p>
+          ) : null}
+<h2>Saved Points</h2>
           <p>
-            <button onClick={handleSaveClick}> 
-              Noktayı Kaydet
-            </button>
+            <button onClick={handleSaveClick}  style={{
+                backgroundColor: "#f5f5f5", 
+                padding: "5px 10px", 
+                borderRadius: "10px", 
+                marginRight: "10px", 
+                marginBottom: "10px", 
+                cursor: "pointer",
+              }}>Noktayı Kaydet</button>
           </p>
           <p>
-            <button onClick={handleDownloadClick}>
-              İndir
-            </button>
+            <button onClick={handleDownloadClick} style={{
+                backgroundColor: "#f5f5f5", 
+                padding: "5px 10px", 
+                borderRadius: "10px", 
+                marginRight: "10px", 
+                marginBottom: "10px", 
+                cursor: "pointer",
+              }} >İndir</button>
           </p>
-          <p>
-            <button>
-              Sil
-            </button>
-          </p>
+          <ListData data={savedPoints} setClickPoint={setClickPoint}/>
+         
         </div>
       </div>
       {displayMap}
     </div>
   );
-}
-
-
+          }
 export default Map;
